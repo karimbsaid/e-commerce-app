7dfba953b538402ff3718c7c076f8926bb0bb2ec
karim ben said <70843037+karimbsaid@users.noreply.github.com>
2025-03-13 04:52:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
+echo "Waiting for PostgreSQL to start..."
+until psql -h "$DB_HOST" -U "$DB_USERNAME" -d "$DB_DATABASE" -c "SELECT 1;" > /dev/null 2>&1; do
+    sleep 3
+done
+
+echo "PostgreSQL is ready. Running migrations..."
 
 # Run migrations
 php artisan migrate --force
+php artisan db:seed --force
 php artisan storage:link
+
 # Set permissions
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 # Start PHP-FPM
-php-fpm -D
-
-# Start Nginx
-nginx -g "daemon off;"
+exec php-fpm
